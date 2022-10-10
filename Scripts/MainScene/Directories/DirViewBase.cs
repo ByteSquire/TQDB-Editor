460189a46faccd5fa182eaaff288eb17cb7bcd23
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-10-10 12:11:07 +0200

    feat(DirViewBase): add auto config, fix selecting root dir

--- Scripts/MainScene/Directories/DirViewBase.cs
+++ Scripts/MainScene/Directories/DirViewBase.cs
@@ -6,195 +6,205 @@ using System.IO;
 using System.Text;
 using System.Transactions;
 
-public abstract partial class DirViewBase : Tree
+namespace TQDBEditor.Files
 {
-    [Export(PropertyHint.NodeType, "Node")]
-    private Config configNode;
-
-    private FileSystemWatcher sourceWatcher;
-
-    private string dirPath;
-
-    private TreeItem root;
-
-    protected abstract string SubPath { get; }
-
-    [Signal]
-    public delegate void DirSelectedEventHandler(string path);
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public abstract partial class DirViewBase : Tree
     {
-        if (configNode is null)
-            return;
-        dirPath = Path.Combine(configNode.ModDir, SubPath);
+        private Config configNode;
 
-        root = CreateItem();
-        root.SetText(0, Path.Combine(configNode.ModName, SubPath));
+        private FileSystemWatcher sourceWatcher;
 
-        ItemSelected += OnDirSelected;
+        private string dirPath;
 
-        if (!Directory.Exists(dirPath))
+        private TreeItem root;
+
+        protected abstract string SubPath { get; }
+
+        [Signal]
+        public delegate void DirSelectedEventHandler(string path);
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
         {
-            GD.PrintErr($"The source directory {dirPath} could not be found, creating...");
-            Directory.CreateDirectory(dirPath);
+            configNode = this.GetEditorConfig();
+            if (configNode is null)
+                return;
+            dirPath = Path.Combine(configNode.ModDir, SubPath);
+
+            root = CreateItem();
+            root.SetText(0, Path.Combine(configNode.ModName, SubPath));
+
+            ItemSelected += OnDirSelected;
+
+            if (!Directory.Exists(dirPath))
+            {
+                GD.PrintErr($"The source directory {dirPath} could not be found, creating...");
+                Directory.CreateDirectory(dirPath);
+            }
+
+            sourceWatcher = new FileSystemWatcher(dirPath)
+            {
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true,
+                NotifyFilter = NotifyFilters.DirectoryName,
+            };
+            sourceWatcher.Created += DirectoryCreated;
+            sourceWatcher.Deleted += DirectoryDeleted;
+            sourceWatcher.Renamed += DirectoryRenamed;
+
+            AddDirectoriesToTree();
         }
 
-        sourceWatcher = new FileSystemWatcher(dirPath)
+        private void AddDirectoriesToTree()
         {
-            IncludeSubdirectories = true,
-            EnableRaisingEvents = true,
-            NotifyFilter = NotifyFilters.DirectoryName,
-        };
-        sourceWatcher.Created += DirectoryCreated;
-        sourceWatcher.Deleted += DirectoryDeleted;
-        sourceWatcher.Renamed += DirectoryRenamed;
-
-        AddDirectoriesToTree();
-    }
-
-    private void AddDirectoriesToTree()
-    {
-        AddDirectoryToTree(root, dirPath);
-    }
-
-    private void AddDirectoryToTree(TreeItem parent, string dirPath)
-    {
-        var paths = Directory.EnumerateDirectories(dirPath, "*", SearchOption.TopDirectoryOnly);
-        //var roots = new Dictionary<TreeItem, string>();
-        foreach (var path in paths)
-        {
-            var nextDir = Path.GetDirectoryName(path);
-            var nodeName = Path.GetRelativePath(nextDir, path);
-            var root = AddChildItem(parent, nodeName);
-
-            AddDirectoryToTree(root, path);
+            AddDirectoryToTree(root, dirPath);
         }
-    }
 
-    private TreeItem AddChildItem(TreeItem parent, string childName)
-    {
-        var newNode = CreateItem(parent);
-        newNode.Collapsed = true;
-        newNode.SetText(0, childName);
-        return newNode;
-    }
-
-    private void AddDir(string dirPath)
-    {
-        (var closest, var remaining) = FindClosestNode(dirPath);
-        //GD.Print("closest " + closest.GetText(0));
-        //GD.Print("remaining:");
-        //GD.Print(remaining);
-        //GD.Print("");
-        foreach (var dir in remaining)
+        private void AddDirectoryToTree(TreeItem parent, string dirPath)
         {
-            closest = AddChildItem(closest, dir);
+            var paths = Directory.EnumerateDirectories(dirPath, "*", SearchOption.TopDirectoryOnly);
+            //var roots = new Dictionary<TreeItem, string>();
+            foreach (var path in paths)
+            {
+                var nextDir = Path.GetDirectoryName(path);
+                var nodeName = Path.GetRelativePath(nextDir, path);
+                var root = AddChildItem(parent, nodeName);
+
+                AddDirectoryToTree(root, path);
+            }
         }
-    }
 
-    protected (TreeItem, string[]) FindClosestNode(string dirPath)
-    {
-        var currDir = root.GetChild(0);
-        var dirs = dirPath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
-
-        int i = 0;
-        var lastDir = currDir;
-        for (; i < dirs.Length - 1; i++)
+        private TreeItem AddChildItem(TreeItem parent, string childName)
         {
-            while (currDir is not null && currDir.GetText(0) != dirs[i])
+            var newNode = CreateItem(parent);
+            newNode.Collapsed = true;
+            newNode.SetText(0, childName);
+            return newNode;
+        }
+
+        private void AddDir(string dirPath)
+        {
+            (var closest, var remaining) = FindClosestNode(dirPath);
+            //GD.Print("closest " + closest.GetText(0));
+            //GD.Print("remaining:");
+            //GD.Print(remaining);
+            //GD.Print("");
+            foreach (var dir in remaining)
+            {
+                closest = AddChildItem(closest, dir);
+            }
+        }
+
+        protected (TreeItem, string[]) FindClosestNode(string dirPath)
+        {
+            var currDir = root.GetChild(0);
+            var dirs = dirPath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+
+            int i = 0;
+            var lastDir = currDir;
+            for (; i < dirs.Length - 1; i++)
+            {
+                while (currDir is not null && currDir.GetText(0) != dirs[i])
+                {
+                    currDir = currDir.GetNext();
+                }
+                if (currDir is null)
+                {
+                    currDir = lastDir;
+                    break;
+                }
+                if (currDir.GetText(0) == dirs[i])
+                {
+                    if (currDir.GetChildCount() == 0)
+                    {
+                        i++;
+                        break;
+                    }
+                    if (i < dirs.Length - 2)
+                    {
+                        lastDir = currDir;
+                        currDir = currDir.GetChild(0);
+                    }
+                }
+            }
+            return (currDir, dirs[i..]);
+        }
+
+        private void RemoveDir(string dirPath)
+        {
+            (var closest, var remaining) = FindClosestNode(dirPath);
+            //GD.Print("closest " + closest.GetText(0));
+            //GD.Print("remaining:");
+            //GD.Print(remaining);
+            //GD.Print("");
+            if (remaining.Length > 1)
+            {
+                GD.PrintErr("Whoops, trying to delete a directory inside a subdirectory that wasn't picked up yet");
+                return;
+            }
+            var currDir = closest.GetChild(0);
+            while (currDir is not null && currDir.GetText(0) != remaining[0])
             {
                 currDir = currDir.GetNext();
             }
             if (currDir is null)
             {
-                currDir = lastDir;
-                break;
+                GD.PrintErr("Whoops, trying to delete a directory that wasn't picked up yet");
+                return;
             }
-            if (currDir.GetText(0) == dirs[i])
+            if (currDir.GetText(0) == remaining[0])
             {
-                if (currDir.GetChildCount() == 0)
-                {
-                    i++;
-                    break;
-                }
-                if (i < dirs.Length - 2)
-                {
-                    lastDir = currDir;
-                    currDir = currDir.GetChild(0);
-                }
+                currDir.Free();
             }
         }
-        return (currDir, dirs[i..]);
-    }
 
-    private void RemoveDir(string dirPath)
-    {
-        (var closest, var remaining) = FindClosestNode(dirPath);
-        //GD.Print("closest " + closest.GetText(0));
-        //GD.Print("remaining:");
-        //GD.Print(remaining);
-        //GD.Print("");
-        if (remaining.Length > 1)
+        private void DirectoryRenamed(object sender, RenamedEventArgs e)
         {
-            GD.PrintErr("Whoops, trying to delete a directory inside a subdirectory that wasn't picked up yet");
-            return;
-        }
-        var currDir = closest.GetChild(0);
-        while (currDir is not null && currDir.GetText(0) != remaining[0])
-        {
-            currDir = currDir.GetNext();
-        }
-        if (currDir is null)
-        {
-            GD.PrintErr("Whoops, trying to delete a directory that wasn't picked up yet");
-            return;
-        }
-        if (currDir.GetText(0) == remaining[0])
-        {
-            currDir.Free();
-        }
-    }
-
-    private void DirectoryRenamed(object sender, RenamedEventArgs e)
-    {
-        RemoveDir(Path.GetRelativePath(dirPath, e.OldFullPath));
-        AddDir(Path.GetRelativePath(dirPath, e.FullPath));
-    }
-
-    private void DirectoryDeleted(object sender, FileSystemEventArgs e)
-    {
-        RemoveDir(Path.GetRelativePath(dirPath, e.FullPath));
-    }
-
-    private void DirectoryCreated(object sender, FileSystemEventArgs e)
-    {
-        AddDir(Path.GetRelativePath(dirPath, e.FullPath));
-    }
-
-    public void OnDirSelected()
-    {
-        var selected = GetSelected();
-        var paths = new List<string>();
-        var parent = selected.GetParent();
-
-        while (parent is not null)
-        {
-            paths.Add(parent.GetText(0));
-            parent = parent.GetParent();
+            RemoveDir(Path.GetRelativePath(dirPath, e.OldFullPath));
+            AddDir(Path.GetRelativePath(dirPath, e.FullPath));
         }
 
-        paths.RemoveAt(paths.Count - 1);
-        paths.Reverse();
-        paths.Add(selected.GetText(0));
+        private void DirectoryDeleted(object sender, FileSystemEventArgs e)
+        {
+            RemoveDir(Path.GetRelativePath(dirPath, e.FullPath));
+        }
 
-        var relativePath = Path.Combine(paths.ToArray());
-        EmitSignal(nameof(DirSelected), Path.Combine(dirPath, relativePath));
-    }
+        private void DirectoryCreated(object sender, FileSystemEventArgs e)
+        {
+            AddDir(Path.GetRelativePath(dirPath, e.FullPath));
+        }
 
-    public override void _ExitTree()
-    {
-        sourceWatcher.Dispose();
-        base._ExitTree();
+        public void OnDirSelected()
+        {
+            var selected = GetSelected();
+            if (selected == root)
+            {
+                EmitSignal(nameof(DirSelected), dirPath);
+                return;
+            }
+            var paths = new List<string>();
+            var parent = selected.GetParent();
+
+            while (parent is not null)
+            {
+                paths.Add(parent.GetText(0));
+                parent = parent.GetParent();
+            }
+            if (paths.Count > 0)
+            {
+                paths.RemoveAt(paths.Count - 1);
+                paths.Reverse();
+            }
+            paths.Add(selected.GetText(0));
+
+            var relativePath = Path.Combine(paths.ToArray());
+            EmitSignal(nameof(DirSelected), Path.Combine(dirPath, relativePath));
+        }
+
+        public override void _ExitTree()
+        {
+            sourceWatcher.Dispose();
+            base._ExitTree();
+        }
     }
 }
