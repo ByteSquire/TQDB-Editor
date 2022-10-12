ca14b7951ac0556a52ef00b72393822c4b1bdbce
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-10-12 09:53:42 +0200

    feat(CreateNewMod): check validity in popup

--- Scripts/MainScene/MenuBar/Management/ModManagement.cs
+++ Scripts/MainScene/MenuBar/Management/ModManagement.cs
@@ -34,7 +34,7 @@ namespace TQDBEditor
             GD.Print("Mod -> New");
 
             var existingList = createModDialog.GetNode<ItemList>("Grid/ExistingMods");
-            var existingMods = Directory.EnumerateDirectories(Path.Combine(config.WorkingDir, "CustomMaps"), "*", SearchOption.TopDirectoryOnly);
+            var existingMods = Directory.EnumerateDirectories(config.ModsDir, "*", SearchOption.TopDirectoryOnly);
             existingList.Clear();
             foreach (var mod in existingMods)
                 existingList.AddItem(Path.GetFileName(mod), selectable: false);
@@ -48,27 +48,7 @@ namespace TQDBEditor
             var modName = lineEdit.Text;
             lineEdit.Clear();
 
-            var additionalInvalidChars = new char[] { '.' };
-            var invalidChars = Path.GetInvalidFileNameChars().Concat(additionalInvalidChars);
-
-            if (modName.Any(x => invalidChars.Contains(x)))
-            {
-                this.GetConsoleLogger()
-                    .LogError("The provided directory name: {modName} for the new mod is invalid!",
-                    modName);
-                _on_mod_new();
-                return;
-            }
-            if (Directory.Exists(Path.Combine(config.WorkingDir, "CustomMaps", modName)))
-            {
-                this.GetConsoleLogger()
-                    .LogError("The provided directory name: {modName} for the new mod already exists!",
-                    modName);
-                _on_mod_new();
-                return;
-            }
-
-            Directory.CreateDirectory(Path.Combine(config.WorkingDir, "CustomMaps", modName));
+            Directory.CreateDirectory(Path.Combine(config.ModsDir, modName));
 
             selectNewModDialog.GetNode<Label>("ModScroll/ModName").Text = modName;
 
---
+++ Scripts/Popups/NewModValidity.cs
@@ -0,0 +1,55 @@
+using Godot;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Linq;
+using TQDBEditor.Scripts;
+
+namespace TQDBEditor
+{
+    public partial class NewModValidity : LineEdit
+    {
+        public override void _Ready()
+        {
+            GetParent().GetParent<ConfirmationDialog>().RegisterTextEnter(this);
+            base._Ready();
+        }
+
+        public void _on_text_changed(string text)
+        {
+            CheckValid();
+        }
+
+        private void CheckValid()
+        {
+            var config = this.GetEditorConfig();
+            var modName = Text;
+            var CreateButton = GetParent().GetParent<ConfirmationDialog>().GetOkButton();
+            if (!PathValidity.IsDirValid(modName))
+            {
+                if (string.IsNullOrEmpty(modName))
+                    modName = "[None]";
+                TooltipText = $"The provided directory name: {modName} for the new mod is invalid!";
+                CreateButton.Disabled = true;
+                AddThemeColorOverride("font_color", Colors.Red);
+                return;
+            }
+            if (Directory.Exists(Path.Combine(config.ModsDir, modName)))
+            {
+                TooltipText = $"The provided directory name: {modName} for the new mod already exists!";
+                CreateButton.Disabled = true;
+                AddThemeColorOverride("font_color", Colors.Red);
+                return;
+            }
+
+            TooltipText = string.Empty;
+            AddThemeColorOverride("font_color", Colors.White);
+            CreateButton.Disabled = false;
+        }
+
+        public void _on_about_to_popup()
+        {
+            CheckValid();
+        }
+    }
+}
