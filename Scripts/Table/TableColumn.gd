caa2a5dcb8238bfb936d8dff0bff79225a84d12f
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-10-25 11:37:45 +0200

    impr(TableColumn): calculate minimum size

--- Scripts/Table/TableColumn.gd
+++ Scripts/Table/TableColumn.gd
@@ -7,18 +7,18 @@ var _cell_height : int
 	set(value):
 		if value != _cell_height:
 			_cell_height = value
-			updated_cell_height()
+			queue_sort()
+
 
 func _notification(what):
 	if what == NOTIFICATION_SORT_CHILDREN:
-		var offset : int = 0
+		var offset : int = _cell_height / 2
 		# Must re-sort the children
 		for c in get_children():
 			# Fit to own size
 			fit_child_in_rect( c, Rect2( Vector2(0, offset), Vector2(size.x, _cell_height) ) )
 			offset += _cell_height
-
-func updated_cell_height():
-	# Some setting changed, ask for children re-sort
-	queue_sort()
 	
+
+func _get_minimum_size():
+	return Vector2i(0, get_child_count() * _cell_height + _cell_height)
