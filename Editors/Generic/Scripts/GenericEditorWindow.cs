6958e79f9c03e5b6bff21c1f0738583162e6712c
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-07 08:56:41 +0100

    fix(GenericEditorWindow): add missing signal connections, easier in code

--- Editors/Generic/Scripts/GenericEditorWindow.cs
+++ Editors/Generic/Scripts/GenericEditorWindow.cs
@@ -9,10 +9,32 @@ using System.Threading.Tasks;
 
 namespace TQDBEditor.EditorScripts
 {
-	public partial class GenericEditorWindow : EditorWindow
-	{
-		protected override void OnClose()
-		{
-		}
-	}
+    public partial class GenericEditorWindow : EditorWindow
+    {
+        public override void _Ready()
+        {
+            base._Ready();
+
+            if (menuBar is EditorMenuBarManager menuBarManager)
+            {
+                menuBarManager.EditEntry += () =>
+                {
+                    var focussed = GuiGetFocusOwner();
+                    if (focussed == null || !focussed.HasMethod("activate"))
+                        return;
+
+                    focussed.Call("activate");
+                };
+                menuBarManager.Find += ShowFind;
+                menuBarManager.Undo += Undo;
+                menuBarManager.Redo += Redo;
+                menuBarManager.Saved += OnFileSaved;
+                menuBarManager.ToggleStatusbar += () => statusBar.Visible = false;
+            }
+        }
+
+        protected override void OnClose()
+        {
+        }
+    }
 }
