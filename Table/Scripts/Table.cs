44f986be96290ab48c1ac9fc6095641074d3a00a
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-09 09:24:54 +0100

    feat(Table): add shift selection and InsertRow. EnumerateColumn methods

--- Table/Scripts/Table.cs
+++ Table/Scripts/Table.cs
@@ -104,10 +104,36 @@ public partial class Table : Control
         var index = GetCellPosition(node).y;
         if (index < 0)
             return;
-        if (!Input.IsKeyPressed(Key.Shift))
+
+        if (Input.IsKeyPressed(Key.Ctrl) || Input.IsKeyPressed(Key.Shift))
+        {
+            if (Input.IsKeyPressed(Key.Shift))
+            {
+                var firstFocussed = GetFocussedRows()[0];
+                var lastFocussed = GetFocussedRows()[^1];
+                if (index < firstFocussed || index > lastFocussed)
+                {
+                    bool up = index < firstFocussed;
+                    var focussed = up ? firstFocussed : lastFocussed;
+
+                    var next = up ? focussed - 1 : focussed + 1;
+                    for (int i = next; i != index && i != focussed;)
+                    {
+                        FocusRow(i);
+                        if (up)
+                            i--;
+                        else
+                            i++;
+                    }
+                }
+            }
+        }
+        else
             foreach (var selectedRowIndex in GetFocussedRows())
                 UnFocusRow(selectedRowIndex);
+
         FocusRow(index);
+        GetViewport().GuiReleaseFocus();
     }
 
     public IReadOnlyList<int> GetFocussedRows()
@@ -115,18 +141,18 @@ public partial class Table : Control
         if (_columns is null || _columns.Count < 1)
             return new List<int>();
 
-        return _columns[0].GetChildren()
+        var ret = _columns[0].GetChildren()
             .Where(x => x.HasMeta("is_focussed"))
-            //.Where(x => (bool)x.GetMeta("is_focussed"))
             .Select(x => GetCellPosition(x as Control).y)
             .ToList();
+        ret.Sort();
+        return ret;
     }
 
     int lastDrag = 0;
 
     public int AddRow(Godot.Collections.Array<Control> values)
     {
-
         if (values.Count != columns.Count)
         {
             GD.PrintErr("You must pass an array that contains a value for every column!");
@@ -137,23 +163,65 @@ public partial class Table : Control
         {
             if (_columns[i].GetChildCount() > 0)
                 _columns[i].AddChild(new HSeparator());
-            values[i].SetMeta("table_cell_position", new Vector2i(i, _columns[i].GetChildCount()));
+            values[i].SetMeta("is_table_cell", true);
             _columns[i].AddChild(values[i]);
         }
 
         return _columns[0].GetChildCount() - 1;
     }
 
+    public int InsertRow(int index, Godot.Collections.Array<Control> values)
+    {
+        if (values.Count != columns.Count)
+        {
+            GD.PrintErr("You must pass an array that contains a value for every column!");
+            return -1;
+        }
+        var myIndex = ConvertToChildIndex(index);
+        if (index < 0 || myIndex > _columns[0].GetChildCount())
+        {
+            GD.PrintErr("You must pass a non-negative index that's in range");
+            return -1;
+        }
+
+        for (int i = 0; i < values.Count; i++)
+        {
+            if (myIndex > 0)
+            {
+                var sep = new HSeparator();
+                _columns[i].AddChild(sep);
+                _columns[i].MoveChild(sep, myIndex - 1);
+            }
+            values[i].SetMeta("is_table_cell", true);
+            _columns[i].AddChild(values[i]);
+            _columns[i].MoveChild(values[i], myIndex);
+        }
+
+        return myIndex;
+    }
+
     public void RemoveRow(int index)
     {
         if (_columns is null)
             return;
+        if (index < 0)
+            return;
 
+        var myIndex = ConvertToChildIndex(index);
         foreach (var column in _columns)
         {
-            if (column.GetChildCount() < index)
+            var numChildren = column.GetChildCount();
+            if (numChildren > index)
             {
-                var child = column.GetChild(index);
+                var child = column.GetChild(myIndex);
+                GD.Print(child.GetIndex());
+                GD.Print(index);
+                if (numChildren > index + 1)
+                {
+                    var childSep = column.GetChild(myIndex + 1);
+                    column.RemoveChild(childSep);
+                    childSep.QueueFree();
+                }
                 column.RemoveChild(child);
                 child.QueueFree();
             }
@@ -167,24 +235,28 @@ public partial class Table : Control
 
         var rowA = GetRow(indexA);
         var rowB = GetRow(indexB);
+        var myIndexA = ConvertToChildIndex(indexA);
+        var myIndexB = ConvertToChildIndex(indexB);
         for (int i = 0; i < _columns.Count; i++)
         {
             var column = _columns[i];
-            column.MoveChild(rowA[i], indexB);
-            column.MoveChild(rowB[i], indexA);
-            rowA[i].SetMeta("table_cell_position", new Vector2i(i, indexB));
-            rowB[i].SetMeta("table_cell_position", new Vector2i(i, indexA));
+            column.MoveChild(rowA[i], myIndexB);
+            column.MoveChild(rowB[i], myIndexA);
+            //rowA[i].SetMeta("table_cell_position", new Vector2i(i, indexB));
+            //rowB[i].SetMeta("table_cell_position", new Vector2i(i, indexA));
         }
     }
 
     public Control GetCellAt(Vector2i cellPosition)
     {
-        return _columns[cellPosition.x].GetChild<Control>(cellPosition.y);
+        var myIndex = ConvertToChildIndex(cellPosition.y);
+        return _columns[cellPosition.x].GetChild<Control>(myIndex);
     }
 
     public Godot.Collections.Array<Control> GetRow(int index)
     {
-        if (_columns[0].GetChildCount() <= index)
+        var myIndex = ConvertToChildIndex(index);
+        if (_columns[0].GetChildCount() <= myIndex)
         {
             GD.PrintErr("The passed index " + index + " is out of range for this table");
             return null;
@@ -192,14 +264,14 @@ public partial class Table : Control
         var ret = new Godot.Collections.Array<Control>();
 
         foreach (var column in _columns)
-            ret.Add(column.GetChild<Control>(index));
+            ret.Add(column.GetChild<Control>(myIndex));
 
         return ret;
     }
 
     public Vector2i GetCellPosition(Control cell)
     {
-        if (!cell.HasMeta("table_cell_position"))
+        if (!cell.HasMeta("is_table_cell"))
         {
             GD.PrintErr("The passed node is not part of a table");
             return new Vector2i(-1, -1);
@@ -207,23 +279,42 @@ public partial class Table : Control
         else
         {
             var parent = cell.GetParent<Container>();
-            if (parent != null && _columns.Contains(parent))
-                return cell.GetMeta("table_cell_position").AsVector2i();
+            if (parent != null)
+            {
+                var cIdx = _columns.IndexOf(parent);
+                if (cIdx >= 0)
+                    return new Vector2i(cIdx, cell.GetIndex() / 2);
+            }
         }
 
         GD.PrintErr("The passed node is not part of this table");
         return new Vector2i(-1, -1);
     }
 
+    public IEnumerable<Control> EnumerateColumn(int index)
+    {
+        if (_columns is null)
+            yield break;
+        if (_columns.Count <= index)
+            yield break;
+
+        var numChildren = _columns[index].GetChildCount();
+        for (int i = 0; i < numChildren; i += 2)
+        {
+            yield return _columns[index].GetChild<Control>(i);
+        }
+    }
+
     public void FocusRow(int index)
     {
         if (_columns is null)
             return;
-        _columns[0].GetChild<Control>(index).GrabFocus();
+        var myIndex = ConvertToChildIndex(index);
+        _columns[0].GetChild<Control>(myIndex).GrabFocus();
 
         foreach (var column in _columns)
         {
-            var cChild = column.GetChild<Control>(index);
+            var cChild = column.GetChild<Control>(myIndex);
             cChild.AddThemeStyleboxOverride("normal", new StyleBoxFlat() { BgColor = Colors.Blue });
             cChild.SetMeta("is_focussed", true);
         }
@@ -234,9 +325,10 @@ public partial class Table : Control
         if (_columns is null)
             return;
 
+        var myIndex = ConvertToChildIndex(index);
         foreach (var column in _columns)
         {
-            var cChild = column.GetChild<Control>(index);
+            var cChild = column.GetChild<Control>(myIndex);
             cChild.RemoveThemeStyleboxOverride("normal");
             cChild.RemoveMeta("is_focussed");
         }
@@ -249,6 +341,8 @@ public partial class Table : Control
         foreach (var column in _columns)
             ClearContainer(column);
     }
+
+    private static int ConvertToChildIndex(int index) => index * 2;
 
     private static void ClearContainer(Container container)
     {
