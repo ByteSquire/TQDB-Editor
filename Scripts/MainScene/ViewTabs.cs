e0216707d0a262eb2b2758e8742dfa7a36e2cbe4
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-10 12:07:38 +0100

    feat(ViewTabs): implement adding views

--- Scripts/MainScene/ViewTabs.cs
+++ Scripts/MainScene/ViewTabs.cs
@@ -1,9 +1,40 @@
 using Godot;
 using System;
+using TQDBEditor.Common;
 
 namespace TQDBEditor
 {
     public partial class ViewTabs : TabContainer
     {
+        private PCKHandler pckHandler;
+
+        public override void _Ready()
+        {
+            var config = this.GetEditorConfig();
+            if (config is null)
+                return;
+
+            config.TrulyReady += Init;
+            if (config.ValidateConfig())
+                Init();
+        }
+
+        private void Init()
+        {
+            pckHandler = this.GetPCKHandler();
+
+            var views = pckHandler.GetViews();
+
+            foreach (var viewTooltip in views)
+            {
+                foreach (var view in viewTooltip.Value)
+                {
+                    var child = view.Instantiate();
+                    var nameExtension = string.Format(" ({0})", viewTooltip.Key);
+                    child.Name += nameExtension;
+                    AddChild(child);
+                }
+            }
+        }
     }
 }
