9e6334bf0882bf11c2f5b1722d43a1699eb2d620
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2023-07-19 16:02:42 +0200

    feat(ClassicView): implement basic directory browser

--- TQDBEditor/ClassicViewModule/ViewModels/ClassicViewViewModel.cs
+++ TQDBEditor/ClassicViewModule/ViewModels/ClassicViewViewModel.cs
@@ -1,29 +1,165 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.Extensions.Logging;
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+using Avalonia.Controls.Templates;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
 using Prism.Ioc;
-using Prism.Regions;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TQDBEditor.Controls;
+using TQDBEditor.Services;
 using TQDBEditor.ViewModels;
+
+using TQDB_Parser.DBRMeta;
+using Microsoft.Extensions.Logging;
 
 namespace TQDBEditor.ClassicViewModule.ViewModels
 {
     public partial class ClassicViewViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private string _greeting = "Hello!";
+        private ObservableCollection<Node> _treeNodes = new();
 
-        public ClassicViewViewModel(IContainerProvider container)
+        private readonly ObservableCollection<MyFileInfos> _files = new();
+        [ObservableProperty]
+        private FlatTreeDataGridSource<MyFileInfos> _dataGridSource;
+
+        private string? _workingDir;
+        private string? WorkingModsFolder => _workingDir is null ? null : Path.Combine(_workingDir, "CustomMaps");
+        private string? _selectedView;
+        private string? _modDir;
+        private readonly ILogger _logger;
+
+        public ClassicViewViewModel(IObservableConfiguration configuration, ILoggerProvider loggerFactory)
         {
-            ;
+            _logger = loggerFactory.CreateLogger(nameof(TQDBEditor.ClassicViewModule));
+            _workingDir = configuration.GetWorkingDir();
+            _modDir = configuration.GetModDir();
+            OnViewSelected("Sources");
+            configuration.AddWorkingDirChangeListener(x => _workingDir = x);
+            configuration.AddModDirChangeListener(OnModDirChanged);
+
+            DataGridSource = CreateBasicDataGridSource();
+        }
+
+        private FlatTreeDataGridSource<MyFileInfos> CreateBasicDataGridSource()
+        {
+            return new FlatTreeDataGridSource<MyFileInfos>(_files)
+            {
+                Columns =
+                {
+                    new TextColumn<MyFileInfos, string>("Name", x => Path.GetFileName(x.FullPath)),
+                    //new TemplateColumn<FileInfo>("Test", new FuncDataTemplate<FileInfo>((_,_) => new Button { Content="Hi" })),
+                }
+            };
+        }
+
+        private void OnModDirChanged(string? path)
+        {
+            Reset();
+            if (path != null && WorkingModsFolder != null && _selectedView != null)
+            {
+                _modDir = path;
+                var modPath = Path.Combine(WorkingModsFolder, _modDir);
+                TreeNodes = InitDirectory(Path.Combine(modPath, _selectedView.ToLower()));
+            }
+        }
+
+        private ObservableCollection<Node> InitDirectory(string sourcePath)
+        {
+            var infos = Directory.CreateDirectory(sourcePath).EnumerateDirectories().ToList();
+            Node root = new(sourcePath, _modDir! + "\\" + _selectedView);
+            var ret = new ObservableCollection<Node>
+            {
+                root
+            };
+            foreach (var info in infos)
+            {
+                root.AddSubNode(new(info.FullName, info.Name, InitDirectory(info.FullName)));
+            }
+            return ret;
+        }
+
+        public void OnNodeSelected(Node? selectedNode)
+        {
+            _files.Clear();
+            if (selectedNode != null)
+                foreach (var info in Directory.CreateDirectory(selectedNode.Path).EnumerateFileSystemInfos().OfType<FileInfo>())
+                    _files.Add(new(info.FullName, _logger));
+        }
+
+        public void OnViewSelected(string? view)
+        {
+            _selectedView = view;
+            OnModDirChanged(_modDir);
+            var dataGridSource = CreateBasicDataGridSource();
+            switch (view)
+            {
+                case "Assets":
+                    dataGridSource.Columns.Add(new TextColumn<MyFileInfos, string>("Status", x => string.Empty));
+                    break;
+                case "Database":
+                    dataGridSource.Columns.Add(new TextColumn<MyFileInfos, string>("Description", x => x.Metadata.GetValueOrDefault().FileDescription));
+                    dataGridSource.Columns.Add(new TextColumn<MyFileInfos, string>("Template", x => x.Metadata.GetValueOrDefault().FileDescription));
+                    break;
+            }
+            DataGridSource = dataGridSource;
+        }
+
+        private void Reset()
+        {
+            TreeNodes.Clear();
+            _files.Clear();
         }
 
         public override string ToString()
         {
             return "Classic (ArtManager)";
+        }
+    }
+
+    public class MyFileInfos
+    {
+        public string FullPath { get; set; }
+        private DBRMetadata? _metadata;
+        private readonly ILogger? _logger;
+        public DBRMetadata? Metadata => _metadata ??= DBRMetaParser.ParseFile(FullPath, _logger);
+
+        public MyFileInfos(string fullPath, ILogger? logger = null)
+        {
+            FullPath = fullPath;
+            _logger = logger;
+        }
+
+    }
+
+    public partial class Node : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Node>? _subNodes;
+        [ObservableProperty]
+        private string _title;
+
+        public string Path { get; }
+
+        public Node(string path, string title)
+        {
+            Path = path;
+            Title = title;
+        }
+
+        public Node(string path, string title, ObservableCollection<Node> subNodes) : this(path, title)
+        {
+            SubNodes = subNodes;
+        }
+
+        public void AddSubNode(Node node)
+        {
+            SubNodes ??= new();
+            SubNodes.Add(node);
         }
     }
 }
--- TQDBEditor/ClassicViewModule/Views/ClassicView.axaml.cs
+++ TQDBEditor/ClassicViewModule/Views/ClassicView.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using System;
+using TQDBEditor.ClassicViewModule.ViewModels;
 
 namespace TQDBEditor.ClassicViewModule.Views
 {
@@ -8,6 +10,32 @@ namespace TQDBEditor.ClassicViewModule.Views
         public ClassicView()
         {
             InitializeComponent();
+            // Always expand first item in Tree
+            Tree.PropertyChanged += (s, args) =>
+            {
+                if (args.Property == ItemsControl.ItemsSourceProperty)
+                {
+                    var treeViewItem = (TreeViewItem?)Tree.ContainerFromIndex(0);
+                    if (treeViewItem != null)
+                        treeViewItem.IsExpanded = true;
+                }
+            };
+        }
+
+        public void OnNodeSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataContext is ClassicViewViewModel viewModel)
+            {
+                viewModel.OnNodeSelected(e.AddedItems[0] as Node);
+            }
+        }
+
+        public void OnTabSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataContext is ClassicViewViewModel viewModel)
+            {
+                viewModel.OnViewSelected((e.AddedItems[0] as TabItem)?.Header?.ToString());
+            }
         }
     }
 }
