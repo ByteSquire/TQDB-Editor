cc7fecedb5d20fe53774a68d5b6eaca7a1f6ac74
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2023-08-29 13:57:04 +0200

    fix(MyDialogService): allow owner override, make sure non-modal children are closed with owner

--- TQDBEditor.Core/Dialogs/MyDialogService.cs
+++ TQDBEditor.Core/Dialogs/MyDialogService.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Prism.Ioc;
 using Prism.Services.Dialogs;
+using System;
 using System.Linq;
 
 namespace TQDBEditor.Dialogs
@@ -13,9 +14,15 @@ namespace TQDBEditor.Dialogs
 
         protected override void ShowDialogWindow(IDialogWindow dialogWindow, bool isModal, Window? owner = null)
         {
-            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+            if (owner == null && Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
             {
                 owner = lifetime.Windows.Where(x => x.IsActive).LastOrDefault();
+            }
+            if (!isModal && owner != null)
+            {
+                EventHandler? closed = null;
+                closed = (_, _) => { dialogWindow.Close(); owner.Closed -= closed; };
+                owner.Closed += closed;
             }
             base.ShowDialogWindow(dialogWindow, isModal, owner);
         }
